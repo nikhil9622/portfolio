a3a843b435f31696e5aff5634fb7b21acb5f1988
Belide Nikhil Chandan <belidenikhilchandan@gmail.com>
2025-11-28 11:27:52 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/portfolio',
   images: {
     domains: ['localhost'],
     unoptimized: true, // Required for static export
